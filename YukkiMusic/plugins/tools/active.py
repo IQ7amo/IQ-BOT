be6e66b3b7d24a8bc4408b90ca705d289504244a
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-12 11:03:50 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -17,14 +17,14 @@ from YukkiMusic.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
 
 # Commands
-ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
-ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
+ACTIVEVC_COMMAND = get_command("فەرمانی چالاککردنی دەنگ")
+ACTIVEVIDEO_COMMAND = get_command("فەرمانی چالاککردنی ڤیدیۆ")
 
 
 @app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
-        "Getting active voice chats.. Please hold"
+        "دەستکەوتنی قسەکردنی دەنگی چالاك ، تکایە بوەستە"
     )
     served_chats = await get_active_chats()
     text = ""
@@ -41,10 +41,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("No Active Voice Chats")
+        await mystic.edit_text("قسەکردنی دەنگی چالاك نییە")
     else:
         await mystic.edit_text(
-            f"**Active Voice Chats:-**\n\n{text}",
+            f"**قسەکردنی دەنگی چالاکە:-**\n\n{text}",
             disable_web_page_preview=True,
         )
 
@@ -52,7 +52,7 @@ async def activevc(_, message: Message):
 @app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text(
-        "Getting active video chats.. Please hold"
+        "دەستکەوتنی ڤیدیۆی دەنگی چالاك، تکایە بوەستە"
     )
     served_chats = await get_active_video_chats()
     text = ""
@@ -69,9 +69,9 @@ async def activevi_(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("No Active Voice Chats")
+        await mystic.edit_text("قسەکردنی دەنگی چالاك نییە")
     else:
         await mystic.edit_text(
-            f"**Active Video Calls:-**\n\n{text}",
+            f"**ڤیدیۆی دەنگی چالاکە:-**\n\n{text}",
             disable_web_page_preview=True,
         )
