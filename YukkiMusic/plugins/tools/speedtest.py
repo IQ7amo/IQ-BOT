11a080775dd407892bf2ff8958a3bb1e3795619d
IQerenn <111300830+IQerenn@users.noreply.github.com>
2022-09-12 11:39:49 +0300

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -15,20 +15,20 @@ from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 
 # Commands
-SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
+SPEEDTEST_COMMAND = get_command("فەرمانی پێوانەکردنی خێرایی")
 
 
 def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("Running Download SpeedTest")
+        m = m.edit("دابەزاندنی پێوانەکردنی خێرایی")
         test.download()
-        m = m.edit("Running Upload SpeedTest")
+        m = m.edit("نوێکردنەوەی پێوانەکردنی خێرایی")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("Sharing SpeedTest Results")
+        m = m.edit("هاوبەشکردنی ئەنجامەکانی پێوانەکردنی خێرایی")
     except Exception as e:
         return m.edit(e)
     return result
@@ -36,24 +36,24 @@ def testspeed(m):
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("Running Speed test")
+    m = await message.reply_text("ڕاکردنی پێوانەکردنی خێرایی")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**Speedtest Results**
+    output = f"""**ئەنجامەکانی پێوانەکردنی خێرایی**
     
 <u>**Client:**</u>
-**__ISP:__** {result['client']['isp']}
-**__Country:__** {result['client']['country']}
+**__دابینکەری خوزمەتگوزاری ئینتەرنێت:__** {result['client']['isp']}
+**__وڵات:__** {result['client']['country']}
   
 <u>**Server:**</u>
-**__Name:__** {result['server']['name']}
-**__Country:__** {result['server']['country']}, {result['server']['cc']}
-**__Sponsor:__** {result['server']['sponsor']}
-**__Latency:__** {result['server']['latency']}  
-**__Ping:__** {result['ping']}"""
+**__ناو:__** {result['server']['name']}
+**__وڵات:__** {result['server']['country']}, {result['server']['cc']}
+**__سپۆنسەر:__** {result['server']['sponsor']}
+**__لێشاو:__** {result['server']['latency']}  
+**__پینگ:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, 
-        photo=result["share"], 
+        photo=result["هاوبەشکردن"], 
         caption=output
     )
     await m.delete()
